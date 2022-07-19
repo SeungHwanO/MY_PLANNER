30f278d21a055dc2139e3800f766903a9e324c3a
j2xxmin <baekjimin0728@gmail.com>
2022-07-19 19:36:41 +0900

    .

---
+++ js/planner.js
@@ -0,0 +1,9 @@
+function login() {
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("pw_filed").value;
+
+    alert("djkd");
+}
+let button = () => {
+    alert('login Button !')
+} 
